87b7720a44859f4fcccfa14e9fd58f656ca8cdb3
abrarsalary <83340914+abrarsalary@users.noreply.github.com>
2021-07-31 09:51:34 +0500

    Formate Posts attributes

--- src/Components/Sale/Attributes/index.js
+++ src/Components/Sale/Attributes/index.js
@@ -2,28 +2,56 @@ import React from "react";
 import Detail from "./Detail";
 import Price from "./Price";
 import Photos from "./Photos";
-import Location from './Location'
+import Location from "./Location";
 import UserDetail from "./UDetail";
 import { useAuth } from "../../../contexts/AuthContext";
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Attributes() {
   const { currentUser } = useAuth();
 
   return (
     <>
-    <Container>
-      <Row>
-      
-      <Col xs={10}> 
-      <Detail />
-      <Price />
-      <Location />
-       </Col>
-      {/* <Photos /> */}
-    {/* <UserDetail /> */}
-    </Row>
-    </Container>
+      <Container
+        style={{
+          // border: "1px solid black",
+          padding: "50px 10px 50px 30px",
+          margin: "50px 50px 50px 50px",
+          alignContent: "center"
+        }}
+      >
+        <Col xs={12}>
+          <Row
+            style={{
+              // margin: "10px 10px 10px 10px",
+              padding: "10px 10px 10px 10px",
+              border: "1px solid black"
+            }}
+          >
+            <Detail />
+          </Row>
+          <Row
+            style={{
+              // margin: "10px 10px 10px 10px",
+              padding: "10px 10px 10px 10px",
+              border: "1px solid black"
+            }}
+          >
+            <Price />
+          </Row>
+          <Row
+            style={{
+              // margin: "10px 10px 10px 10px",
+              padding: "10px 10px 10px 10px",
+              border: "1px solid black"
+            }}
+          >
+            <Location />
+          </Row>
+        </Col>
+        {/* <Photos /> */}
+        {/* <UserDetail /> */}
+      </Container>
     </>
   );
 }
