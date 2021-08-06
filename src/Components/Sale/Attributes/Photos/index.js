7a5e073cf205acf9078f885562fc4a9aa76c0372
abrarsalary <83340914+abrarsalary@users.noreply.github.com>
2021-08-06 12:36:42 +0500

    working with photos module

--- src/Components/Sale/Attributes/Photos/index.js
+++ src/Components/Sale/Attributes/Photos/index.js
@@ -5,24 +5,48 @@ import IconButton from "@material-ui/core/IconButton";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 import { useStyles } from "./style";
 import { Container, Typography } from "@material-ui/core";
-import Hidden from "@material-ui/core/Hidden";
-import UploadImage from "./UploadImage";
+
 export default function Photos() {
   const classes = useStyles();
 
   return (
-    <div>
+    <div htmlFor="click">
       <Container maxWidth="xl" className={classes.parentContainer}>
         <Typography variant="h6" className={classes.PhotosHeading}>
           UPLOAD UP TO 10 PHOTOS
         </Typography>
       </Container>
       <Container className={classes.root} maxWidth="xl">
-        <Grid container spacing={1} htmlFor="fileUpload">
-          <label>
-            <UploadImage />
-            <input hidden id="fileUpload" type="file" accept="video/*" />
-          </label>
+        <Grid item xs={2} sm={1}>
+          <Paper
+            className={classes.paper}
+            variant="outlined"
+            style={{ border: "1px solid black" }}
+          >
+            <label>
+              click mi
+              <input type="file" id="click" className={classes.input} />
+              <AddAPhotoIcon htmlFor="icon-button-file">
+                <IconButton
+                  color="primary"
+                  aria-label="upload picture"
+                  component="span"
+                >
+                  {/* <input
+                  accept="image/*"
+                  // className={classes.input}
+                  id="icon-button-file"
+                  type="file"
+                /> */}
+                  {/* <IconButton
+                  color="primary"
+                  aria-label="upload picture"
+                  component="span"
+                /> */}
+                </IconButton>
+              </AddAPhotoIcon>
+            </label>
+          </Paper>
         </Grid>
       </Container>
     </div>
