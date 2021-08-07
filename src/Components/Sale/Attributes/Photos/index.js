a1cd0f97df48b69a12cb0e24f2a67af5d253c3db
abrarsalary <83340914+abrarsalary@users.noreply.github.com>
2021-08-07 17:11:07 +0500

    continue working with Photos Module with Storage

--- src/Components/Sale/Attributes/Photos/index.js
+++ src/Components/Sale/Attributes/Photos/index.js
@@ -1,38 +1,133 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useStyles } from "./style";
 import { Container, Typography } from "@material-ui/core";
 import Images from "./Images";
+import { storage, database } from "../../../../firebase";
 import { v4 as uuidV4 } from "uuid";
 
 export default function Photos() {
   const classes = useStyles();
 
+  // useEffect(() => {
+  //   window.addEventListener("beforeunload", deleteImages);
+  //   return () => {
+  //     window.removeEventListener("beforeunload", deleteImages);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", alertUser);
+    window.addEventListener("unload", handleTabClosing);
+    return () => {
+      window.removeEventListener("beforeunload", alertUser);
+      window.removeEventListener("unload", handleTabClosing);
+    };
+  });
+
+  const handleTabClosing = () => {
+    // removePlayerFromGame();
+    // alert("clicl");
+    deleteImages();
+  };
+
+  const alertUser = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+    deleteImages();
+  };
+
+  // this code to delte images from storage
+  const deleteImages = () => {
+    storage
+      .ref(`images/`)
+      .listAll()
+      .then((listResults) => {
+        const promises = listResults.items.map((item) => {
+          return item.delete();
+        });
+        Promise.all(promises);
+        setUrls([]);
+      });
+  };
   const [uploadingFiles, setUploadingFiles] = useState([]);
 
-  const handleUpload = (e) => {};
+  const [urls, setUrls] = useState([]);
 
-  const imagesData = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ];
+  const handleUpload = (e) => {
+    const file = e.target.files[0];
+    if (file == null) return;
+
+    const id = uuidV4();
+    setUploadingFiles((prevUploadingFiles) => [
+      ...prevUploadingFiles,
+      { id: id, name: file.name, progress: 0, error: false }
+    ]);
+
+    const filePath = `/images/${file.name}`;
+
+    const uploadTask = storage.ref(`${filePath}`).put(file);
+    // console.log(uploadTask);
+    uploadTask.on(
+      "state_changed",
+
+      //progress code
+      (snapshot) => {
+        const progress = snapshot.bytesTransferred / snapshot.totalBytes;
+        // console.log(progress);
+        setUploadingFiles((prevUploadingFiles) => {
+          return prevUploadingFiles.map((uploadFile) => {
+            if (uploadFile.id === id) {
+              return { ...uploadFile, progress: progress };
+            }
+            return uploadFile;
+          });
+        });
+      },
+      // error code
+      () => {
+        setUploadingFiles((prevUploadingFiles) => {
+          return prevUploadingFiles.map((uploadFile) => {
+            if (uploadFile.id === id) {
+              return { ...uploadFile, error: true };
+            }
+            return uploadFile;
+          });
+        });
+      },
+
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+          setUrls([...urls, url]);
+        });
+      }
+    );
+  };
+
+  // const imagesData = [
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   "",
+  //   ""
+  // ];
+
+  // console.log(uploadingFiles);
+  console.log(urls);
   return (
     <div>
       <Container maxWidth="xl" className={classes.parentContainer}>
@@ -48,18 +143,24 @@ export default function Photos() {
             className={classes.input}
             onChange={handleUpload}
             multiple="multiple"
+            accept="image/*"
           />
 
-          {imagesData.length > 0
+          {<Images imgSrc={urls} />}
+          {/* {imagesData.length > 0
             ? imagesData.map((arr, index) => {
                 if (index === 10) {
                   <br />;
                 }
                 return <Images key={index} imgSrc={arr} />;
               })
-            : ""}
+            : ""} */}
+          {/* {uploadingFiles.map((arr, index) => {
+            return <Images key={index} imgSrc={arr} />;
+          })} */}
         </Container>
       </label>
+      <button onClick={deleteImages}>click me </button>
     </div>
   );
 }
